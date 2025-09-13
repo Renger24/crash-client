f6153e322d1228011c836ed46e4413ae4561e109
Renger24 <97744390+Renger24@users.noreply.github.com>
2025-09-13 16:46:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ class CrashGame {
 
     connectToServer() {
         // Замените на URL вашего сервера после деплоя
-        const serverUrl = 'https://ваш-сервер.vercel.app';
+        const serverUrl = 'https://crash-client-omega.vercel.app/';
         
         try {
             this.socket = io(serverUrl, {
